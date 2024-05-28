9561481e113ebc9142a7a4ce39b4b761f2b66f94
Muaz Khalid <itzmajo786@gmail.com>
2024-05-28 19:02:33 +0500

    Call component completed

---
+++ src/components/Call.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "@/app/globals.css";
+import { MdLocalPhone } from "react-icons/md";
+
+const Call = () => {
+  return (
+    <div className="bg-black call flex  p-0 lg:pl-60 items-center">
+      <div className="text-center textarea1">
+        <div className="py-10 text-3xl text-white">Any Question You Have</div>
+        <div className="font-bold  text-6xl lg:text-8xl md:text-7xl text-white">
+          +158 862 1252
+        </div>
+        <div className=" flex justify-center mt-10">
+          <div className="bg-orange-500 font-bold hover:bg-blue-600  ease-in-out duration-500 flex gap-2 text-white rounded-full  py-5 w-40 justify-center text-center">
+            <div>
+              <MdLocalPhone className=" m-0 p-0 text-xl" />
+            </div>
+            MAKE CALL
+          </div>
+        </div>
+      </div>
+      <div className="call-img-cont hidden  md:hidden lg:block">
+        <img src="/call.png" className="call-img" />
+      </div>
+    </div>
+  );
+};
+
+export default Call;
