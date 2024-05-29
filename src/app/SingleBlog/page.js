9e0a464c2ec3cd869695793b23e00c920aa833db
Muaz Khalid <itzmajo786@gmail.com>
2024-05-29 12:51:37 +0500

    working on Blog page

--- src/app/SingleBlog/page.js
+++ src/app/SingleBlog/page.js
@@ -25,109 +25,213 @@ const Page = () => {
       </div>
 
       <div className="flex flex-col lg:flex-row mx-2 my-20">
-        <div className="p1 border mx-auto lg:mx-0">
-          <div>
-            <img src="/1.jpg" className="w-full" alt="Blog Main" />
+        <div className="p1 mx-auto lg:mx-0">
+          <div className="border">
+            <div>
+              <img src="/1.jpg" className="w-full" alt="Blog Main" />
+            </div>
+
+            <div className="flex gap-5 my-10 ml-5">
+              <div className="flex items-center">
+                <MdOutlineDateRange className="text-xl text-orange-500" />
+                <p className="text-sm text-center ml-2">24 May 2024</p>
+              </div>
+              <div className="flex items-center">
+                <FaTags className="text-xl text-orange-500" />
+                <a
+                  href="#"
+                  className="text-sm text-blue-700 hover:text-orange-500 ease-in-out duration-500 ml-2"
+                >
+                  Technology
+                </a>
+              </div>
+            </div>
+            <div className="mx-5">
+              <p className="text-3xl px-0 font-bold">
+                What’s the Holding Back the It Solution Industry?
+              </p>
+
+              <p className="mt-10">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry’s standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries.
+              </p>
+              <p className="mt-10">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry’s standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries.
+              </p>
+            </div>
+
+            <div className="flex justify-center gap-2 px-2 lg:justify-start flex-col content-center lg:flex-row mt-5">
+              <img
+                src="/2.jpg"
+                className="blog-img h-72 mb-2 mx-auto"
+                alt="Blog Image 2"
+              />
+              <img
+                src="/3.jpg"
+                className="blog-img h-72  mb-2 mx-auto "
+                alt="Blog Image 3"
+              />
+            </div>
+
+            <div className="mx-5">
+              <p className="mt-10">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry’s standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries.
+              </p>
+              <p className="mt-10">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry’s standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries.
+              </p>
+            </div>
+
+            <div className="mx-5 my-5">
+              <div className="flex gap-3 font-medium">
+                <span className="font-bold py-1">Tags:</span>
+                <span className="px-3 border py-1 hover:text-white hover:bg-orange-500  ease-in-out duration-500  bg-slate-100">
+                  HTML5
+                </span>
+                <span className="px-3 text-center hover:text-white hover:bg-orange-500 py-1 border bg-slate-100 ease-in-out duration-500 ">
+                  IT
+                </span>
+                <span className="px-3 hover:text-white hover:bg-orange-500   text-center py-1 border bg-slate-100 ease-in-out duration-500 ">
+                  Technology
+                </span>
+              </div>
+              <div className="flex gap-3 mt-5">
+                <span className="font-bold py-1">Share:</span>
+                <span className="px-3 border py-1 hover:text-white hover:bg-orange-500  ease-in-out duration-500 ">
+                  <FiLinkedin className="text-md" />
+                </span>
+                <span className="px-3 text-center hover:text-white hover:bg-orange-500 py-1 border  ease-in-out duration-500 ">
+                  <FiInstagram className="text-md" />
+                </span>
+                <span className="px-3 hover:text-white hover:bg-orange-500   text-center py-1 border  ease-in-out duration-500 ">
+                  <FiFacebook className="text-md" />
+                </span>
+                <span className="px-3 hover:text-white hover:bg-orange-500   text-center py-1 border  ease-in-out duration-500 ">
+                  <FiTwitter className="text-md" />
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="comment border w-full mx-auto mt-5 lg:mx-0">
+            <div className=" font-bold text-2xl pl-3 py-4">03 Comments</div>
+            <div className="mb-10">
+              <div className="px-4 flex py-5">
+                <div className="com-img">
+                  <img src="/user.jpg" className="h-24 w-24 md:w-32 lg:w-60" />
+                </div>
+                <div className="px-4 com flex flex-col">
+                  <span className="font-medium">Jonathom Doe</span>
+                  <span className="text-sm leading-loose">
+                    September 19, 2021 at 11:25 am
+                  </span>
+                  <span className="text-sm leading-loose">
+                    Aenean volutpat sed lacus eu faucibus. Quisque nunc dui,
+                    finibus at bibendum et, egestas eget lectus. Ut iaculis
+                    tempor risus a dictum. Aenean volutpat sed lacus eu
+                    faucibus. Quisque nunc dui, finibus at bibendum et, egestas
+                    eget lectus. Ut iaculis tempor risus a dictum.
+                  </span>
+                  <a className="border rounded-full px-5 w-20 py-1 hover:text-orange-500 ease-in-out duration-500 hover:border-orange-500 mt-5">
+                    Reply
+                  </a>
+                </div>
+              </div>
+              <div className="pr-4  pl-32 flex py-5">
+                <div className="com-img">
+                  <img src="/user.jpg" className="h-24 w-24 md:w-32 lg:w-60" />
+                </div>
+                <div className="px-4 com flex flex-col">
+                  <span className="font-medium">Jonathom Doe</span>
+                  <span className="text-sm leading-loose">
+                    September 19, 2021 at 11:25 am
+                  </span>
+                  <span className="text-sm leading-loose">
+                    Aenean volutpat sed lacus eu faucibus. Quisque nunc dui,
+                    finibus at bibendum et, egestas eget lectus. Ut iaculis
+                    tempor risus a dictum.
+                  </span>
+                  <a className="border rounded-full px-5 w-20 py-1 hover:text-orange-500 ease-in-out duration-500 hover:border-orange-500 mt-5">
+                    Reply
+                  </a>
+                </div>
+              </div>
+              <div className="px-4 flex py-5">
+                <div className="com-img">
+                  <img src="/user.jpg" className="h-24 w-24 md:w-32 lg:w-60" />
+                </div>
+                <div className="px-4 com flex flex-col">
+                  <span className="font-medium">Jonathom Doe</span>
+                  <span className="text-sm leading-loose">
+                    September 19, 2021 at 11:25 am
+                  </span>
+                  <span className="text-sm leading-loose">
+                    Aenean volutpat sed lacus eu faucibus. Quisque nunc dui,
+                    finibus at bibendum et, egestas eget lectus. Ut iaculis
+                    tempor risus a dictum.
+                  </span>
+                  <a className="border rounded-full px-5 w-20 py-1 hover:text-orange-500 ease-in-out duration-500 hover:border-orange-500 mt-5">
+                    Reply
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
 
-          <div className="flex gap-5 my-10 ml-5">
-            <div className="flex items-center">
-              <MdOutlineDateRange className="text-xl text-orange-500" />
-              <p className="text-sm text-center ml-2">24 May 2024</p>
+          <div className="comment border px-3 py-10 w-full mx-auto mt-5 lg:mx-0">
+            <div>
+              <span className="font-bold text-2xl ">
+                Leave Your Comment Here
+              </span>
             </div>
-            <div className="flex items-center">
-              <FaTags className="text-xl text-orange-500" />
-              <a
-                href="#"
-                className="text-sm text-blue-700 hover:text-orange-500 ease-in-out duration-500 ml-2"
-              >
-                Technology
+
+            <div className="border my-5">
+              <textarea
+                className="w-full px-4  h-40 py-2 text-sm"
+                placeholder="Write Comment"
+              ></textarea>
+            </div>
+            <div className="border my-5">
+              <input
+                className="px-4 py-4 w-full"
+                placeholder="Your Name"
+              ></input>
+            </div>
+            <div className="border my-5">
+              <input
+                className="px-4 py-4 w-full"
+                placeholder="Your Email"
+              ></input>
+            </div>
+            <div className="border my-5">
+              <input
+                className="px-4 py-4 w-full"
+                placeholder="Your Subjects"
+              ></input>
+            </div>
+            <div className="py-5">
+              <a className=" text-white font-bold text-1xl bg-orange-500 px-5  py-4">
+                Submit Comment
               </a>
-            </div>
-          </div>
-          <div className="mx-5">
-            <p className="text-3xl px-0 font-bold">
-              What’s the Holding Back the It Solution Industry?
-            </p>
-
-            <p className="mt-10">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry’s standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries.
-            </p>
-            <p className="mt-10">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry’s standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries.
-            </p>
-          </div>
-
-          <div className="flex justify-center lg:justify-start flex-col content-center lg:flex-row mt-5">
-            <img
-              src="/2.jpg"
-              className="blog-img h-72 mb-2 mx-auto"
-              alt="Blog Image 2"
-            />
-            <img
-              src="/3.jpg"
-              className="blog-img h-72  mb-2 mx-auto "
-              alt="Blog Image 3"
-            />
-          </div>
-
-          <div className="mx-5">
-            <p className="mt-10">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry’s standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries.
-            </p>
-            <p className="mt-10">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry’s standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries.
-            </p>
-          </div>
-
-          <div className="mx-5 my-5">
-            <div className="flex gap-3 font-medium">
-              <span className="font-bold py-1">Tags:</span>
-              <span className="px-3 border py-1 hover:text-white hover:bg-orange-500  ease-in-out duration-500  bg-slate-100">
-                HTML5
-              </span>
-              <span className="px-3 text-center hover:text-white hover:bg-orange-500 py-1 border bg-slate-100 ease-in-out duration-500 ">
-                IT
-              </span>
-              <span className="px-3 hover:text-white hover:bg-orange-500   text-center py-1 border bg-slate-100 ease-in-out duration-500 ">
-                Technology
-              </span>
-            </div>
-            <div className="flex gap-3 mt-5">
-              <span className="font-bold py-1">Share:</span>
-              <span className="px-3 border py-1 hover:text-white hover:bg-orange-500  ease-in-out duration-500 ">
-                <FiLinkedin className="text-md" />
-              </span>
-              <span className="px-3 text-center hover:text-white hover:bg-orange-500 py-1 border  ease-in-out duration-500 ">
-                <FiInstagram className="text-md" />
-              </span>
-              <span className="px-3 hover:text-white hover:bg-orange-500   text-center py-1 border  ease-in-out duration-500 ">
-                <FiFacebook className="text-md" />
-              </span>
-              <span className="px-3 hover:text-white hover:bg-orange-500   text-center py-1 border  ease-in-out duration-500 ">
-                <FiTwitter className="text-md" />
-              </span>
             </div>
           </div>
         </div>
 
-        <div className="p2 text-center bg-red-400">asd</div>
+        <div className="p2 text-center ">asd</div>
       </div>
     </>
   );
