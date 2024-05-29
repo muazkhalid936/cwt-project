1f6dbf435e11476390dad1a04fea3c67bf7aef4e
Muaz Khalid <itzmajo786@gmail.com>
2024-05-30 02:52:53 +0500

    Blog Page Done

--- src/app/SingleBlog/page.js
+++ src/app/SingleBlog/page.js
@@ -27,7 +27,7 @@ const Page = () => {
       </div>
 
       <div className="flex flex-col gap-5 justify-center px-0 md:px-10 lg:mx-auto lg:flex-row mx-2 my-20">
-        <div className="w-p1 mx-auto lg:mx-0">
+        <div className="w-4/3 lg:w-p1 mx-auto lg:mx-0">
           <div className="border">
             <div>
               <img src="/1.jpg" className="w-full" alt="Blog Main" />
@@ -361,6 +361,89 @@ const Page = () => {
                     <div className="text-sm datee">18 Sep 2021</div>
                   </div>
                 </div>
+              </div>
+            </div>
+          </div>
+
+          <div className=" border w-full">
+            <div className=" flex my-5">
+              <div className=" mt-2 w-3 h-2 bg-black"></div>{" "}
+              <h2 className="ml-5 font-medium">Tags</h2>
+            </div>
+            <div className=" py-5 flex flex-wrap gap-2 mx-10  items-center px-auto ">
+              <a className="px-3 border bg-slate-100 ease-in-out duration-500 py-2 hover:bg-orange-500 hover:text-white text-sm font-bold">
+                HTML
+              </a>
+              <a className="px-3 border bg-slate-100 ease-in-out duration-500 py-2 hover:bg-orange-500 hover:text-white text-sm font-bold">
+                DESIGN
+              </a>
+              <a className="px-3 border bg-slate-100 ease-in-out duration-500 py-2 hover:bg-orange-500 hover:text-white text-sm font-bold">
+                WORDPRESS
+              </a>
+              <a className="px-3 border bg-slate-100 ease-in-out duration-500 py-2 hover:bg-orange-500 hover:text-white text-sm font-bold">
+                INTERIOR
+              </a>
+              <a className="px-3 border bg-slate-100 ease-in-out duration-500 py-2 hover:bg-orange-500 hover:text-white text-sm font-bold">
+                APARTMENTS
+              </a>
+              <a className="px-3 border bg-slate-100 ease-in-out duration-500 py-2 hover:bg-orange-500 hover:text-white text-sm font-bold">
+                DEVELOPMENT
+              </a>
+              <a className="px-3 border bg-slate-100 ease-in-out duration-500 py-2 hover:bg-orange-500 hover:text-white text-sm font-bold">
+                PHP
+              </a>
+              <a className="px-3 border bg-slate-100 ease-in-out duration-500 py-2 hover:bg-orange-500 hover:text-white text-sm font-bold">
+                PHOTOGRAPHY
+              </a>
+              <a className="px-3 border bg-slate-100 ease-in-out duration-500 py-2 hover:bg-orange-500 hover:text-white text-sm font-bold">
+                PROPERTY
+              </a>
+              <a className="px-3 border bg-slate-100 ease-in-out duration-500 py-2 hover:bg-orange-500 hover:text-white text-sm font-bold">
+                MANAGMENT
+              </a>
+            </div>
+          </div>
+
+          <div className=" border w-full">
+            <div className=" flex my-5">
+              <div className=" mt-2 w-3 h-2 bg-black"></div>{" "}
+              <h2 className="ml-5 font-medium">Search</h2>
+            </div>
+            <div class="mx-10  flex flex-col ">
+              <div className="flex mb-1 mt-2 aa hover:text-orange-500 ease-in-out duration-500 ">
+                <div>
+                  {" "}
+                  <FaAngleDoubleRight className="mt-1" />
+                </div>
+                <div className="ml-1 mb-3">September 2021</div>
+              </div>
+              <div className="flex mb-1 mt-2 aa hover:text-orange-500 ease-in-out duration-500 ">
+                <div>
+                  {" "}
+                  <FaAngleDoubleRight className="mt-1" />
+                </div>
+                <div className="ml-1 mb-3">October 2021</div>
+              </div>
+              <div className="flex mb-1 mt-2 aa hover:text-orange-500 ease-in-out duration-500 ">
+                <div>
+                  {" "}
+                  <FaAngleDoubleRight className="mt-1" />
+                </div>
+                <div className="ml-1 mb-3">August 2021</div>
+              </div>
+              <div className="flex mb-1 mt-2 aa hover:text-orange-500 ease-in-out duration-500 ">
+                <div>
+                  {" "}
+                  <FaAngleDoubleRight className="mt-1" />
+                </div>
+                <div className="ml-1 mb-3">July 2021</div>
+              </div>
+              <div className="flex mb-1 mt-2 aa hover:text-orange-500 ease-in-out duration-500 ">
+                <div>
+                  {" "}
+                  <FaAngleDoubleRight className="mt-1" />
+                </div>
+                <div className="ml-1 mb-3">June 2021</div>
               </div>
             </div>
           </div>
