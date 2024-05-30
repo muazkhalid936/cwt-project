f007753ae4fe3242ed934d95d5ce594f1266aa51
Muaz Khalid <itzmajo786@gmail.com>
2024-05-30 15:00:31 +0500

    Contact Page Done

--- src/app/Contact/page.js
+++ src/app/Contact/page.js
@@ -2,8 +2,8 @@ import React from "react";
 import { FaArrowRightLong } from "react-icons/fa6";
 import { LiaMapMarkedAltSolid } from "react-icons/lia";
 import { IoMailOutline } from "react-icons/io5";
-import { RiMailOpenLine } from "react-icons/ri";
-
+import { CiHeadphones } from "react-icons/ci";
+import "./styles.css"
 const Page = () => {
   return (
     <>
@@ -20,6 +20,45 @@ const Page = () => {
         </div>
       </div>
 
+     <div className="flex justify-center">
+
+     <div className="mt-20 grid  gap-10 md:grid-cols-2 lg:grid-cols-3 mx-auto justify-center">
+        <div className="flex gap-2 aa">
+          <div className="bg-orange-100 justify-center flex w-20">
+            <LiaMapMarkedAltSolid className="text-7xl text-orange-500" />
+          </div>
+          <div className="flex flex-col gap-1 justify-end">
+            <span className="font-bold">ADDRESS</span>
+            <span className="text-sm">Kurfürstendamm 80 Mecklenburg Vorpommern Berlin</span>
+          </div>
+        </div>
+        <div className="flex gap-2 aa">
+          <div className="bg-orange-100 w-20 flex justify-center">
+            <CiHeadphones className="text-7xl text-orange-500" />
+          </div>
+          <div className="flex flex-col gap-1 justify-end">
+            <span className="font-bold">OUR PHONE</span>
+            <div className="flex flex-col">
+            <span className="text-sm">Telephone: (+78) 123-9123</span>
+            <span className="text-sm">Mobile: 000 123 9123</span>
+            </div>
+          </div>
+        </div>
+        <div className="flex gap-2 aa">
+          <div className="bg-orange-100 flex justify-center w-20">
+            <IoMailOutline className="text-7xl text-orange-500" />
+          </div>
+          <div className="flex flex-col gap-1 justify-end">
+            <span className="font-bold">EMAIL</span>
+           <div className="flex flex-col">
+           <span className="text-sm">Office Email:123@gmail.com</span>
+            <span className="text-sm">Inquiries:123@gmail.com</span>
+           </div>
+          </div>
+        </div>
+      </div>
+     </div>
+
       <div className="my-20 px-4 mx-auto w-full md:w-2/3 lg:w-1/2 ">
         <div className=" flex justify-center mx-auto">
           <div className="comment px-3 py-10 w-full mx-auto mt-5 lg:mx-0">
@@ -32,7 +71,7 @@ const Page = () => {
                 sit amet maximus est.
               </span>
             </div>
-            
+
             <div className="my-5">
               <label className=" font-medium"> Your Name</label>
               <div className="border">
@@ -59,13 +98,12 @@ const Page = () => {
             </div>
             <div className="py-4 flex justify-center w-full">
               <a className=" flex justify-center w-full text-white text-center font-bold text-1xl hover:bg-blue-500 ease-in-out duration-500 bg-orange-500 px-10  py-4">
-              Send Message
-              <FaArrowRightLong className=" mt-1 ml-2" />
+                Send Message
+                <FaArrowRightLong className=" mt-1 ml-2" />
               </a>
             </div>
           </div>
         </div>
-
       </div>
     </>
   );
