80e586408f508899722281c510d855ed86142b73
Muaz Khalid <itzmajo786@gmail.com>
2024-06-03 00:40:47 +0500

    Still fixing issue

--- src/components/TestCard.js
+++ src/components/TestCard.js
@@ -13,9 +13,9 @@ const TestimonialCard = ({ imageSrc, name, role, rating, testimonial }) => {
   };
 
   return (
-    <div className="relative flex-col bg-white shadow-md max-w-lg my-8 py-6 w-[120vh]  px-4 h-[55vh] flex items-center">
-      <div className="mt-10">
-        <div className="absolute -left-10 mt-10  transform -translate-y-1/2">
+    <div className="relative flex-col bg-white shadow-md max-w-lg my-8 py-6 w-[50vh] lg:w-[120vh]  px-4 h-[40vh] lg:h-[55vh] flex items-center">
+      <div className=" mt-1 lg:mt-10">
+        <div className="absolute -left-6 lg:-left-10 mt-10  transform -translate-y-1/2">
           <Image
             src={imageSrc}
             alt={name}
@@ -25,7 +25,7 @@ const TestimonialCard = ({ imageSrc, name, role, rating, testimonial }) => {
           />
         </div>
 
-        <div className="-ml-40">
+        <div className="ml-0 lg:-ml-40">
           <h2 className="text-lg fontt font-bold">{name}</h2>
           <p className="text-gray-500 mb-2">{role}</p>
           <div className="text-yellow-500 mb-2">{renderStars(rating)}</div>
