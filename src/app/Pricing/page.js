5ca2a0b345513bfbbc71075b18c07ee5ad3d451e
Muaz Khalid <itzmajo786@gmail.com>
2024-06-06 17:51:54 +0500

    fix pricing

--- src/app/Pricing/page.js
+++ src/app/Pricing/page.js
@@ -27,12 +27,12 @@ const Page = () => {
         </div>
 <div className="flex justify-center">
 
-        <div className="py-10 grid xl:grid-cols-3 lg:grid-cols-2 grid-cols-1 gap-3 ">
+        <div className="py-10 grid  lg:grid-cols-3 grid-cols-1 gap-3 ">
          
          
 
-        <div className="h-[60vh] border asd hover:-translate-y-5 group ease-in-out overflow-hidden group duration-500 w-[45vh] ">
-        <div className=" ss h-[40vh] w-[54vh] -ml-10 bg-orange-100 ease-in-out duration-500 group-hover:bg-orange-500 overflow-hidden -mt-60 rounded-full z-10 group"></div>
+        <div className="h-[70vh] lg:h-[100vh] border asd hover:-translate-y-5 group ease-in-out overflow-hidden group duration-500 w-[50vh] ">
+        <div className=" ss h-[40vh] w-[54vh] -ml-3 bg-orange-100 ease-in-out duration-500  -mt-44 lg:-mt-24 group-hover:bg-orange-500 overflow-hidden  rounded-full z-10 group"></div>
         <div className="flex flex-col z-20 -mt-28">
           <span className="text-orange-500 text-center ease-in-out duration-500 group-hover:text-white text-xl pp">Basic</span>
           <span className="text-3xl mt-2 pp font-black ease-in-out duration-500 group-hover:text-white text-center">$19.89</span>
@@ -60,9 +60,9 @@ const Page = () => {
 
 
 
-      <div className="h-[60vh] border asd hover:-translate-y-5 ease-in-out duration-500 w-[45vh] overflow-hidden">
-        <div className=" bg-orange-500 h-[40vh] w-[54vh] -ml-10  -mt-60 rounded-full z-10 group"></div>
-        <div className="flex flex-col z-20 -mt-28">
+      <div className=" h-[70vh] lg:h-[100vh] border asd hover:-translate-y-5 ease-in-out duration-500 w-[50vh] overflow-hidden">
+        <div className=" bg-orange-500 h-[40vh] w-[54vh] -ml-3  -mt-44 lg:-mt-24 rounded-full z-10 group"></div>
+        <div className="flex flex-col z-20 -mt-28 ">
           <span className=" text-center text-xl text-white">Standard</span>
           <span className="mt-2 text-3xl text-white font-black text-center">$99.89</span>
         </div>
@@ -91,8 +91,8 @@ const Page = () => {
 
 
 
-      <div className="h-[60vh] border asd hover:-translate-y-5 ease-in-out group duration-500 w-[45vh] overflow-hidden">
-        <div className=" ss h-[40vh] w-[54vh] -ml-10  -mt-60 rounded-full bg-orange-100 group-hover:bg-orange-500 ease-in-out duration-500  z-10 group"></div>
+      <div className="h-[70vh] lg:h-[100vh] border asd hover:-translate-y-5 ease-in-out group duration-500 w-[50vh] overflow-hidden">
+        <div className=" ss h-[40vh] w-[54vh] -ml-3 -mt-44 lg:-mt-24 rounded-full bg-orange-100 group-hover:bg-orange-500 ease-in-out duration-500  z-10 group"></div>
         <div className="flex flex-col z-20 -mt-28">
           <span className="text-orange-500 group-hover:text-white ease-in-out duration-500 text-center text-xl pp">Premium</span>
           <span className="text-3xl group-hover:text-white ease-in-out duration-500 pp font-black text-center mt-2">$119.89</span>
